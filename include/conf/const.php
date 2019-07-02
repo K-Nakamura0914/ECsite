a41cbc36c6d1dffac730173f52cb47d90b65a9ae
KenshinNakamura <kenshinnakamura@KenshinNakamuranoMacBook-Pro.local>
2019-07-02 16:59:43 +0900

    fix

--- include/conf/const.php
+++ include/conf/const.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST',   'us-cdbr-iron-east-02.cleardb.net'); // データベースのホスト名又はIPアドレス
-define('DB_USER',   'b6186433d6983a');  // MySQLのユーザ名
-define('DB_PASSWD', '148e917b');    // MySQLのパスワード
+define('DB_USER',   'bd36d60e537f5a');  // MySQLのユーザ名
+define('DB_PASSWD', 'df1ca5cc');    // MySQLのパスワード
 define('DB_NAME',   'heroku_ca76823e400e09f');    // データベース名
 
 define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
